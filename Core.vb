b8101372786a0e86bd486fcc9744353d4c0adca3
Peter Swinkels <peter_swinkels@hotmail.com>
2024-07-30 20:56:00 +0200

    Add files via upload

--- Core.vb
+++ Core.vb
@@ -4,6 +4,7 @@ Option Explicit On
 Option Infer Off
 Option Strict On
 
+Imports mshtml
 Imports System
 Imports System.Collections.Generic
 Imports System.Diagnostics
@@ -74,61 +75,98 @@ Public Module CoreModule
             End If
          End If
       Catch ExceptionO As Exception
-         Console.WriteLine(ExceptionO.Message)
+         Console.Error.WriteLine(ExceptionO.Message)
       End Try
    End Sub
 
    'This procedure checks for frames in the specified HTML document.
    Private Sub CheckForFrames(DocumentO As HTMLDocumentStr)
-      Dim Frame As mshtml.HTMLDocument = Nothing
-      Dim FrameIndex() As Integer = Nothing
-      Dim NextFrame As mshtml.HTMLDocument = Nothing
-      Dim Parents() As mshtml.HTMLDocument = Nothing
-      Dim Level As Integer = 0
+      Try
+         Dim Frame As mshtml.HTMLDocument = Nothing
+         Dim FrameIndex() As Integer = Nothing
+         Dim NextFrame As mshtml.HTMLDocument = Nothing
+         Dim Parents() As mshtml.HTMLDocument = Nothing
+         Dim Level As Integer = 0
 
-      ReDim FrameIndex(0 To Level)
-      ReDim Parents(0 To Level)
-      Frame = DocumentO.Document
-      Do Until (Level = 0) AndAlso (FrameIndex(Level) >= Frame.frames.length)
-         Do While FrameIndex(Level) < Frame.frames.length
-            NextFrame = DirectCast(DirectCast(Frame.frames.item(CObj(FrameIndex(Level))), mshtml.HTMLWindow2).document, mshtml.HTMLDocument)
-            If NextFrame Is Nothing Then Exit Do
-            Level += 1
-            ReDim Preserve FrameIndex(0 To Level)
-            ReDim Preserve Parents(0 To Level)
-            Parents(Level) = Frame
-            Frame = NextFrame
+         ReDim FrameIndex(0 To Level)
+         ReDim Parents(0 To Level)
+         Frame = DocumentO.Document
+         Do Until (Level = 0) AndAlso (FrameIndex(Level) >= Frame.frames.length)
+            Do While FrameIndex(Level) < Frame.frames.length
+               NextFrame = DirectCast(DirectCast(Frame.frames.item(CObj(FrameIndex(Level))), mshtml.HTMLWindow2).document, mshtml.HTMLDocument)
+               If NextFrame Is Nothing Then Exit Do
+               Level += 1
+               ReDim Preserve FrameIndex(0 To Level)
+               ReDim Preserve Parents(0 To Level)
+               Parents(Level) = Frame
+               Frame = NextFrame
+            Loop
+
+            If NextFrame Is Nothing Then
+               HTMLDocuments.Add(New HTMLDocumentStr With {.Document = Frame, .Elements = GetDocumentElements(Frame)})
+            Else
+               HTMLDocuments.Add(New HTMLDocumentStr With {.Document = Frame, .Elements = GetDocumentElements(Frame)})
+               Frame = Parents(Level)
+               Level -= 1
+               ReDim Preserve FrameIndex(0 To Level)
+               ReDim Preserve Parents(0 To Level)
+            End If
+
+            If FrameIndex(Level) < Frame.frames.length Then FrameIndex(Level) += 1
          Loop
-
-         If NextFrame Is Nothing Then
-            HTMLDocuments.Add(New HTMLDocumentStr With {.Document = Frame, .Elements = GetDocumentElements(Frame)})
-         Else
-            HTMLDocuments.Add(New HTMLDocumentStr With {.Document = Frame, .Elements = GetDocumentElements(Frame)})
-            Frame = Parents(Level)
-            Level -= 1
-            ReDim Preserve FrameIndex(0 To Level)
-            ReDim Preserve Parents(0 To Level)
-         End If
-
-         If FrameIndex(Level) < Frame.frames.length Then FrameIndex(Level) += 1
-      Loop
+      Catch ExceptionO As Exception
+         Console.Error.WriteLine(ExceptionO.Message)
+      End Try
    End Sub
+
+   'This procedure displays the elements contained by the specified HTML documents.
+   Private Sub DisplayDocuments(HTMLDocuments As List(Of HTMLDocumentStr))
+      Try
+         For Each DocumentO As HTMLDocumentStr In HTMLDocuments
+            Console.WriteLine(DocumentO.Executable)
+            Console.WriteLine(DocumentO.Document.title)
+            For Each ElementO As HTMLElementStr In DocumentO.Elements
+               Console.WriteLine($"<{ElementO.Name}>")
+               For Each AttributeO As HTMLAttributeStr In ElementO.Attributes
+                  Console.WriteLine($"{AttributeO.Name} = {AttributeO.Value}")
+               Next AttributeO
+            Next ElementO
+         Next DocumentO
+      Catch ExceptionO As Exception
+         Console.Error.WriteLine(ExceptionO.Message)
+      End Try
+   End Sub
+
+   'This procedure attempts to enter debug mode and returns the result.
+   Private Function EnterDebugMode() As Boolean
+      Try
+         Process.EnterDebugMode()
+
+         Return True
+      Catch ExceptionO As Exception
+         Console.Error.WriteLine(ExceptionO.Message)
+      End Try
+
+      Return False
+   End Function
 
    'This procedure returns any elements contained by the specified document.
    Private Function GetDocumentElements(DocumentO As mshtml.HTMLDocument) As List(Of HTMLElementStr)
-      Dim Elements As New List(Of HTMLElementStr)
-
       Try
+         Dim Elements As New List(Of HTMLElementStr)
+
          With DocumentO.all
             For ItemIndex As Integer = 0 To .length - 1
-               GetItemElements(Elements, .item(CObj(ItemIndex)))
+               GetItemElements(Elements, DirectCast(.item(ItemIndex), IHTMLElement))
             Next ItemIndex
          End With
+
+         Return Elements
       Catch ExceptionO As Exception
-         Console.WriteLine(ExceptionO.Message)
+         Console.Error.WriteLine(ExceptionO.Message)
       End Try
 
-      Return Elements
+      Return Nothing
    End Function
 
    'This procedure handles any child windows that are found.
@@ -136,7 +174,7 @@ Public Module CoreModule
       Try
          CheckForDocument(hWnd)
       Catch ExceptionO As Exception
-         Console.WriteLine(ExceptionO.Message)
+         Console.Error.WriteLine(ExceptionO.Message)
       End Try
 
       Return CInt(True)
@@ -149,7 +187,7 @@ Public Module CoreModule
 
          EnumChildWindows(hWnd, AddressOf HandleChildWindow, Nothing)
       Catch ExceptionO As Exception
-         Console.WriteLine(ExceptionO.Message)
+         Console.Error.WriteLine(ExceptionO.Message)
       End Try
 
       Return CInt(True)
@@ -158,24 +196,44 @@ Public Module CoreModule
    'This procedue is executed when this program is started.
    Public Sub Main()
       Try
-         HTMLDocuments = New List(Of HTMLDocumentStr)
+         Dim InDebugMode As New Boolean
 
-         Process.EnterDebugMode()
-         EnumWindows(AddressOf HandleWindow, CInt(0))
-         Process.LeaveDebugMode()
+         If Environment.GetCommandLineArgs.Last.Trim() = "/?" Then
+            Console.WriteLine(ProgramInformation())
+            Console.WriteLine()
+            Console.WriteLine(My.Application.Info.Description)
+         Else
+            HTMLDocuments = New List(Of HTMLDocumentStr)
 
-         For Each DocumentO As HTMLDocumentStr In HTMLDocuments
-            Console.WriteLine(DocumentO.Executable)
-            Console.WriteLine(DocumentO.Document.title)
-            For Each ElementO As HTMLElementStr In DocumentO.Elements
-               Console.WriteLine($"<{ElementO.Name}>")
-               For Each AttributeO As HTMLAttributeStr In ElementO.Attributes
-                  Console.WriteLine($"{AttributeO.Name} = {AttributeO.Value}")
-               Next AttributeO
-            Next ElementO
-         Next DocumentO
+            InDebugMode = EnterDebugMode()
+            EnumWindows(AddressOf HandleWindow, CInt(0))
+            If InDebugMode Then Process.LeaveDebugMode()
+
+            If HTMLDocuments.Count = 0 Then
+               Console.WriteLine("No HTML documents found.")
+            Else
+               DisplayDocuments(HTMLDocuments)
+            End If
+         End If
       Catch ExceptionO As Exception
-         Console.WriteLine(ExceptionO.Message)
+         Console.Error.WriteLine(ExceptionO.Message)
       End Try
    End Sub
+
+   'This procedure returns information about this program.
+   Private Function ProgramInformation() As String
+      Try
+         Dim Information As String = Nothing
+
+         With My.Application.Info
+            Information = $"{ .Title} v{ .Version} - by: { .CompanyName}"
+         End With
+
+         Return Information
+      Catch ExceptionO As Exception
+         Console.Error.WriteLine(ExceptionO.Message)
+      End Try
+
+      Return Nothing
+   End Function
 End Module
--- Elements.vb
+++ Elements.vb
@@ -6,6 +6,7 @@ Option Compare Binary
 Option Explicit On
 Option Infer Off
 
+Imports mshtml
 Imports System
 Imports System.Collections.Generic
 Imports System.Linq
@@ -13,21 +14,24 @@ Imports System.Linq
 'This module handles HTML elements.
 Public Module ElementsModule
    'This procedure returns the specified item's name and attributes.
-   Public Sub GetItemElements(Elements As List(Of HTMLElementStr), Item As Object)
+   Public Sub GetItemElements(Elements As List(Of HTMLElementStr), Item As IHTMLElement)
       Try
+         Dim Attribute As IHTMLDOMAttribute = Nothing
          Dim Value As String = Nothing
 
          Elements.Add(New HTMLElementStr With {.Attributes = New List(Of HTMLAttributeStr), .Name = Item.tagName})
-         If Item.Attributes IsNot Nothing Then
-            For Nodeindex As Integer = 0 To Item.Attributes.Length - 1
-               Value = Item.Attributes(Nodeindex).nodeValue?.ToString()
+
+         If Item.attributes IsNot Nothing Then
+            For Nodeindex As Integer = 0 To Item.attributes.length - 1
+               Attribute = CType(Item.attributes.item(Nodeindex), IHTMLDOMAttribute)
+               Value = Attribute.nodeValue?.ToString()
                If Not Value = Nothing Then
-                  Elements.Last().Attributes.Add(New HTMLAttributeStr With {.Name = Item.Attributes(Nodeindex).nodeName, .Value = Value})
+                  Elements.Last().Attributes.Add(New HTMLAttributeStr With {.Name = Attribute.nodeName, .Value = Value})
                End If
             Next Nodeindex
          End If
       Catch ExceptionO As Exception
-         Console.WriteLine(ExceptionO.Message)
+         Console.Error.WriteLine(ExceptionO.Message)
       End Try
    End Sub
 End Module
